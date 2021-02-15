ce36bd9081de067fb9eb0de56c2fffc0a01dd7c7
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-02-15 10:58:47 +0300

    Added new constants

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,5 +7,8 @@ namespace Business.Constants
         public static string CarDailyPriceInvalid = "Araç günlük ücreti geçersiz";
         public static string CarAddedSuccessfully = "Araç başarıyla eklendi";
         public static string CarsListed = "Araçlar başarıyla listelendi";
+        public static string UsersListed = "Kullanıcılar başarıyla listelendi";
+        public static string UserInvalid = "Kullanıcı geçersiz";
+        public static string UserAddedSuccessfully = "Kullanıcı başarıyla eklendi";
     }
 }
