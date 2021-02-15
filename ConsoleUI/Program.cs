fa8a5209efe22ba3293f354d90be32675f6a27b6
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-02-15 10:59:13 +0300

    Tested new service on console

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,6 +8,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //GetCarDetails();
+            //GetUserList();
+        }
+
+        private static void GetUserList()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            foreach (var item in userManager.GetAll().Data)
+            {
+                Console.WriteLine("First Name: " + item.FirstName);
+                Console.WriteLine("Last Name: " + item.LastName);
+                Console.WriteLine("____________________________");
+            }
+        }
+
+        private static void GetCarDetails()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var item in carManager.GetCarDetails().Data)
