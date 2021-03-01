41e7d47f11c00e4d31131142cbb7a7e5b9004522
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-02 00:59:18 +0300

    Implemented data access layer

---
+++ DataAccess/Concrete/EntityFramework/EfCarImageDal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarImageDal : EfEntityRepositoryBase<CarImage, ReCapContext>, ICarImageDal
+    {
+    }
+}
