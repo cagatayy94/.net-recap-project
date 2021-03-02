495a1e20cf4e8ba3de18c8035673735b55c8a76a
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-03 00:56:06 +0300

    Finalized CarImagesController

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Business.Abstract;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
@@ -27,9 +28,22 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
-        [HttpGet("add")]
-        public IActionResult Add(CarImage carImage)
+        [HttpPost("add")]
+        public IActionResult Add([FromForm] CarImage carImage)
         {
+            var imageName = Guid.NewGuid();
+            var image = carImage.Image;
+            if (image.Length > 0)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/images", imageName + ".png");
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(fileStream);
+                }
+            };
+
+            carImage.ImagePath = imageName + ".png";
+
             var result = _carImageService.Add(carImage);
             if (result.Success)
             {
