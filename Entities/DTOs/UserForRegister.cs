73240ff2ded291eb022c1cbdd9d48507a801a19b
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-06 20:51:44 +0300

    Created new dtos for login and register

---
+++ Entities/DTOs/UserForRegister.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class UserForRegister:IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
