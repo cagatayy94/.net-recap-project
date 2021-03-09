04089c90e046efef073c8865eab148f51d32194a
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-09 23:45:49 +0300

    Added car validator

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Business.Abstract;
 using Business.BusinessAspect.Autofac;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -38,6 +40,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == colorId));
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         [SecuredOperation("car.add")]
         public IResult Add(Car car)
         {
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.Description).MinimumLength(3);
+            RuleFor(c => c.ModelYear).Must(GraterThanTwentyYear).WithMessage("Araç 20 yaşından büyük olmamalıdır");
+        }
+
+        private bool GraterThanTwentyYear(int arg)
+        {
+            if (arg<1990)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
