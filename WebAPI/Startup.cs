e7ed253bd77504dd842b50404ca7acd151f6c586
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-14 00:56:04 +0300

    Revert "Updated cors policy"
    This reverts commit d0e323c12e4df167a4ebd630a231b0d27277e2d8.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,7 +41,7 @@ namespace WebAPI
             services.AddControllers();
             services.AddCors(options =>
                 options.AddPolicy("AllowOrigin",
-                    builder=>builder.WithOrigins("http://localhost")
+                    builder=>builder.WithOrigins("http://localhost:53298")
                 )
             );
 
@@ -82,7 +82,7 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:53298").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
