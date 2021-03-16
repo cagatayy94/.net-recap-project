28c1fa420b5d00696b915c8fd1b3632997a4770e
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-17 00:04:17 +0300

    Implemented data access layer

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -9,5 +9,6 @@ namespace DataAccess.Abstract
     public interface ICarDal:IEntityRepository<Car>
     {
         public List<CarDetailDto> GetCarDetails();
+        public List<CarDetailDto> GetCarDetailsByColorIdAndBrandId(int? colorId, int? brandId);
     }
 }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -20,13 +20,50 @@ namespace DataAccess.Concrete.EntityFramework
                                 on car.BrandId equals b.BrandId
                              select new CarDetailDto
                              {
+                                 CarId = car.CarId,
+                                 BrandId = car.BrandId,
+                                 BrandName = b.BrandName,
+                                 ColorId = car.ColorId,
+                                 ColorName = color.ColorName,
+                                 ModelYear = car.ModelYear,
+                                 DailyPrice = car.DailyPrice,
+                                 Description = car.Description,
+                             };
+                return result.ToList();
+            }
+        }
+
+        public List<CarDetailDto> GetCarDetailsByColorIdAndBrandId(int? colorId, int? brandId)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var result = from car in context.Cars
+                             join color in context.Colors
+                             on car.ColorId equals color.ColorId
+                             join b in context.Brands
+                             on car.BrandId equals b.BrandId
+                             select new CarDetailDto
+                             {
                                 CarId= car.CarId,
+                                BrandId = car.BrandId,
                                 BrandName = b.BrandName,
+                                ColorId= car.ColorId,
                                 ColorName = color.ColorName,
                                 ModelYear = car.ModelYear,
                                 DailyPrice = car.DailyPrice,
-                                Description = car.Description
+                                Description = car.Description,
                              };
+
+                if (brandId != null)
+                {
+                    result = result.Where(c=>c.BrandId == brandId);
+                }
+
+                if (colorId != null)
+                {
+                    result = result.Where(c => c.ColorId == colorId);
+                }
+
                 return result.ToList();
             }
         }
