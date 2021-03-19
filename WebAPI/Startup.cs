aba7fbcfbe23cb352af94b4a64a645fdb8bcee3b
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-19 20:55:20 +0300

    commented jwt area

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -47,20 +47,20 @@ namespace WebAPI
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
-                options => {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidIssuer = tokenOptions.Issuer,
-                        ValidAudience = tokenOptions.Audience,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
-                    };
-                }
-            );
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+            //    options => {
+            //        options.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuer = true,
+            //            ValidateAudience = true,
+            //            ValidateLifetime = true,
+            //            ValidIssuer = tokenOptions.Issuer,
+            //            ValidAudience = tokenOptions.Audience,
+            //            ValidateIssuerSigningKey = true,
+            //            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+            //        };
+            //    }
+            //);
 
             services.AddDependencyResolvers(new ICoreModule[] {
                 new CoreModule()
