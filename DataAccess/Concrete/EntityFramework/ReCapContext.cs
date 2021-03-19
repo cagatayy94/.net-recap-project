2c875ac65a55a2cba9bac31ab4b8d91a0b36a0ae
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-19 20:05:48 +0300

    Updated connection string

--- DataAccess/Concrete/EntityFramework/ReCapContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -13,7 +13,8 @@ namespace DataAccess.Concrete.EntityFramework
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=localhost;database=ReCap;User ID=sa;Password=reallyStrongPwd123;");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=ReCap;Trusted_Connection=False;MultipleActiveResultSets=true;user id=sa;password=reallyStrongPwd123");
+
         }
 
         public DbSet<Car> Cars { get; set; }
